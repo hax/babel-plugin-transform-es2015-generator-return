670402ec499810a805d2a71492da4913c9576088
HE Shi-Jun <hax@heshijun.net>
2016-06-19 04:24:34 +0800

    adjust test infrastructure, drop babel-helper-plugin-test-runner

--- test/index.js
+++ test/index.js
@@ -1,1 +1,37 @@
-require("babel-helper-plugin-test-runner")(__dirname)
+import { join }	from 'path'
+import { readdirSync, readFileSync, statSync }	from 'fs'
+import { transformFileSync }	from 'babel-core'
+import plugin	from '../src'
+import assert	from 'assert'
+
+describe('Patch Generator.prototype.return', () => {
+	const fixturesDir = join(__dirname, 'fixtures')
+	for (const caseName of readdirSync(fixturesDir)) {
+		const fixtureDir = join(fixturesDir, caseName)
+		if (!statSync(fixtureDir).isDirectory()) continue
+
+		it(`should ${caseName.split('-').join(' ')}`, () => {
+			const actualPath = join(fixtureDir, 'actual.js')
+			if (existsFile(actualPath)) {
+				const actual = transformFileSync(actualPath).code
+				const expectedPath = join(fixtureDir, 'expected.js')
+				const expected = readFileSync(expectedPath, 'utf-8')
+
+				assert.equal(actual.trim(), expected.trim())
+			}
+			const execPath = join(fixtureDir, 'exec.js')
+			if (existsFile(execPath)) {
+				const exec = transformFileSync(execPath).code
+				new Function('assert', exec)(assert)
+			}
+		})
+	}
+})
+
+function existsFile(path) {
+	try {
+		return statSync(actualPath).isFile()
+	} catch (e) {
+		return false
+	}
+}
